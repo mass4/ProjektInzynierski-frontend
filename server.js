04e5d72510a1ac75bbf1aec79f1af94aa4eabb65
Marek <mareszy279@student.polsl.pl>
2019-12-03 02:22:39 +0100

    fix build and server.js

--- server.js
+++ server.js
@@ -15,7 +15,15 @@
 // console.log('Console listening!');
 
 const express = require('express');
+const http = require('http');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8040);
+const port = process.env.PORT || 3001;
+
+app.use(express.static(__dirname + '/dist/my-app'));
+app.get('/*', (req,res) => res.sendFile(path.join(__dirname)));
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log('Running...'));
